a1482d30eb92d97ff7e771d7334ee77086f40f02
bimonestle <bimonestle@yahoo.co.id>
2021-03-31 22:47:26 +0700

    exercise 4

---
+++ 05. Functions/ex_04_01.py
@@ -0,0 +1,12 @@
+hours = float(input("Enter Hours: "))
+rate = float(input("Enter rate: "))
+def computePay(hours, rate):
+    pay = hours * rate
+    if hours > 40 :
+        basicPay = 40 * rate
+        gap = hours - 40
+        pay = (gap) * rate * 1.5 + basicPay
+        print(pay)
+    else :
+        print(pay)
+computePay(hours, rate)
