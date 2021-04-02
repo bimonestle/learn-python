db7b7b104339a955d3e803d7c28c6706dbfe1096
bimonestle <bimonestle@yahoo.co.id>
2021-04-02 13:32:41 +0700

    fix exercise 5.1

--- 06. Iterations/ex_05_01.py
+++ 06. Iterations/ex_05_01.py
@@ -1,16 +1,17 @@
-def calcNumb():
-    count = 0
-    total = 0.0
-    while True:
-        number = input("Enter a number: ")
-        if number == "done":
-            break
-        try:
-            number = float(number)
-            total += number
-            count += 1
-        except:
-            print("Invalid input")
-    print("Total: ", total, "Count: ", count, "Average: ", total/count)
+# def calcNumb():
+count = 0
+total = 0.0
+while True:
+    number = input("Enter a number: ")
+    if number == "done":
+        break
+    try:
+        number = float(number)
+    except:
+        print("Invalid input")
+        continue
+    total += number
+    count += 1
+print("Total: ", total, "Count: ", count, "Average: ", total/count)
 
-calcNumb()
+# calcNumb()
