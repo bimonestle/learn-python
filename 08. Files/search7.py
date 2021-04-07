b8b3cc7abee022d4c96498e687d16f12461d1577
bimonestle <bimonestle@yahoo.co.id>
2021-04-07 21:12:34 +0700

    Files training

---
+++ 08. Files/search7.py
@@ -0,0 +1,12 @@
+fName = input("Enter file name: ")p
+
+try:
+    fHand = open(fName)
+except:
+    print("File can't be opened:", fName)
+    exit()
+count = 0
+for line in fHand:
+    if line.startswith("Subject:"):
+        count += 1
+print("There were", count, "subject lines in", fName)
