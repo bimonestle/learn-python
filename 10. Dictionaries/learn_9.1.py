48e594ad10b720f0d312f93144ef8459834c10f3
bimonestle <bimonestle@yahoo.co.id>
2021-04-14 19:45:02 +0700

    count occurence of words in file

---
+++ 10. Dictionaries/learn_9.1.py
@@ -0,0 +1,21 @@
+fName = input("Enter file name: ")
+
+try:
+    fHand = open(fName)
+except:
+    print("File cannot be opened:", fName)
+    exit()
+
+counts = dict()
+for line in fHand:
+    words = line.split()
+    for word in words:
+        if word not in counts:
+            counts[word] = 1
+        else:
+            counts[word] += 1
+
+        # using get() to achieve the same result
+        counts[word] = counts.get(word, 0) + 1
+
+print(counts)
