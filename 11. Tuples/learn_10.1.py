699cbf26670ab3e5e2a34b98aa8f8657efceece1
bimonestle <bimonestle@yahoo.co.id>
2021-04-17 16:40:29 +0700

    add new comment

--- 11. Tuples/learn_10.1.py
+++ 11. Tuples/learn_10.1.py
@@ -1,6 +1,9 @@
 txt = 'but soft what light in yonder window breaks'
 words = txt.split()
 t = list()
+
+# Building a list of tuples, where each tuple
+# is a word preceded by its length
 for word in words:
     # add each words and its length to array 't'
     t.append((len(word), word))
@@ -12,6 +15,9 @@ t.sort(reverse=True)
 
 print(t)
 res = list()
+
+# The second loop traverses the list of tuples and
+# builds a list of words in descending order of length.
 for length, word in t:
     # add sorted word based on its length to array 'res'
     res.append(word)
