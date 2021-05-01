a3eaa4d16946ced05d055adab7c54d8c3116e96c
bimonestle <bimonestle@yahoo.co.id>
2021-05-01 13:14:33 +0700

    learn 12.3

---
+++ 13. Network/learn_12.3.py
@@ -0,0 +1,11 @@
+# import urllib.request
+import urllib
+
+# Once the web page has been opened with 'urllib.urlopen',
+# we can treat it like a file and read through it using a 'for' loop.
+fHand = urllib.request.urlopen("http://data.pr4e.org/romeo.txt")
+for line in fHand:
+    # When the program runs, we only see the output of the contents of the file.
+    # The headers are still sent, but the 'urllib' code consumes the headers
+    # and only returns the data to us.
+    print(line.decode().strip())
