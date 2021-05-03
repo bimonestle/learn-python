2d924d1f91f7da2ef2e8eec05576e09b47a4ebae
bimonestle <bimonestle@yahoo.co.id>
2021-05-03 22:38:46 +0700

    testing ex 12.1

---
+++ 13. Network/ex_12.1.py
@@ -0,0 +1,25 @@
+# Change the socket program socket1.py to prompt the user for the URL
+# so it can read any web page. You can use split('/') to break the URL
+# into its component parts so you can extract the host name for the socket connect call.
+# Add error checking using try and except to handle the condition
+# where the user enters an improperly formatted or non-existent URL.
+
+import socket
+
+url = input("Enter url: ")
+# print(url.split("/"))
+urlSplit = url.split("/")
+mySock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mySock.connect((urlSplit[2], 80))
+cmd = "GET " + url + " HTTP/1.0\r\n\r\n"
+cmd = cmd.encode()
+
+mySock.send(cmd)
+
+while True:
+    data = mySock.recv(520)
+    if len(data) < 1:
+        break
+    print(data.decode(), end="")
+
+mySock.close()
