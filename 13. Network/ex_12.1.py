316edc3217c70d1c03d62f37a28db7c529abe593
bimonestle <bimonestle@yahoo.co.id>
2021-05-06 23:00:10 +0700

    add comments for ex 12.1

--- 13. Network/ex_12.1.py
+++ 13. Network/ex_12.1.py
@@ -11,17 +11,30 @@ url = input("Enter url: ")
 
 try:
     urlSplit = url.split("/")
+
+    # Create a socket
     mySock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     hostname = urlSplit[2]
+
+    # Make a socket call to domain and port
     mySock.connect((hostname, 80))
 except:
     print(url, "is an invalid url")
     exit()
 
+# Initialize a command to a 'cmd' variable
 cmd = "GET " + url + " HTTP/1.0\r\n\r\n"
+
+# The command which was a Unicode format is converted
+# to a UTF-8 format by the encode() function
 cmd = cmd.encode()
+
+# Send the command using socket
 mySock.send(cmd)
 while True:
+    # A single character takes exactly 8 bit to stored. So that is 1B (byte).
+    # So the socket ask for some certain numbers of byte
+    # for the character you want to receive (hence it's called recv())
     data = mySock.recv(520)
     if len(data) < 1:
         break
