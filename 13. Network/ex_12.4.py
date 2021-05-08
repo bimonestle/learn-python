5fdb6060bde98ed83c0f0a309c5c2e83fc2f2718
bimonestle <bimonestle@yahoo.co.id>
2021-05-08 16:09:13 +0700

    change comment

--- 13. Network/ex_12.4.py
+++ 13. Network/ex_12.4.py
@@ -20,7 +20,7 @@ url = input("Enter Url - ")
 html = urllib.request.urlopen(url, context=ctx).read()
 soup = BeautifulSoup(html, "html.parser")
 
-# Retrieve all of the anchor tags
+# Retrieve all of the <p> or paragraph tags
 tags = soup("p")
 count = 0
 for tag in tags:
