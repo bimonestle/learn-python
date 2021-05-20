82b9c5496ac7a05e09c34d28a7de1b311b26e643
bimonestle <bimonestle@yahoo.co.id>
2021-05-20 13:53:40 +0700

    Learn 15.1

---
+++ 16. Database/learn_15.1.py
@@ -0,0 +1,21 @@
+import sqlite3
+from sqlite3.dbapi2 import connect
+
+# Makes a connection to the database stored in the file 'music.sqlite'
+# in the current directory. If the file doesn't exist, it will be created.
+# The reason this is called a 'connection' is that sometimes the database
+# is stored on a separate 'database server' from the server on which
+# we are running our application.
+conn = sqlite3.connect('music.sqlite')
+
+# A cursor is like a file handle that we can use to perform operations
+# on the data stored in the database. Calling cursor() is similar conceptually
+# to calling open() when dealing with text files.
+# Or in a nutshell it's like a gateway to a collection of CLI
+# of database commands.
+cur = conn.cursor()
+
+cur.execute('DROP TABLE IF EXISTS Tracks')
+cur.execute('CREATE TABLE Tracks(title TEXT, plays INTEGER)')
+
+cur.close()
