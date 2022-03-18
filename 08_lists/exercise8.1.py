1bf84c4d4bc43ece9490330ed08148f2bd0d59ca
bimonestle <bimonestle@yahoo.co.id>
2022-03-18 18:02:30 +0700

    add explanation comment and delete imported library

--- 08_lists/exercise8.1.py
+++ 08_lists/exercise8.1.py
@@ -1,4 +1,7 @@
-from curses.ascii import isalpha
+# Write a function called chop that takes a list and modifies it,
+# removing the first and last elements, and returns None.
+# Then write a function called middle that takes a list and
+# returns a new list that contains all but the first and last elements.
 
 
 def chop(l: list) -> None:
