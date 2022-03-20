ebe152c4bfb6320319a0e6ebd0042eeb0d39b9cf
bimonestle <bimonestle@yahoo.co.id>
2022-03-20 20:10:20 +0700

    add compound logical expression

---
+++ 08_lists/exercise8.3.py
@@ -0,0 +1,20 @@
+# Rewrite the guardian code in the above example without two if statements.
+# Instead, use a compound logical expression using the or logical operator
+# with a single if statement.
+
+fname = input('Enter file name: ')
+try:
+    fhand = open(fname)
+except:
+    print("Cannot open file name %s" % fname)
+    quit()
+
+count = 0
+for line in fhand:
+    words = line.split()
+    # print('Debug:', words)
+
+    if len(words) < 3 or words[0] != 'From':
+        continue
+    
+    print(words[2])
