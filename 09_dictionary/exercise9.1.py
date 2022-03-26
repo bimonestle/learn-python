e64ae360c29220027fcee45a5c260483fcd08224
bimonestle <bimonestle@yahoo.co.id>
2022-03-26 15:23:22 +0700

    Store keys in a dictionary

---
+++ 09_dictionary/exercise9.1.py
@@ -0,0 +1,21 @@
+# Write a program that reads the words in words.txt and stores them as keys in a dictionary.
+# It doesn’t matter what the values are. Then you can use the in operator as a fast way
+# to check whether a string is in the dictionary.
+
+fName = input("Enter a file name: ")
+
+try:
+    fHand = open(fName)
+except:
+    print("No file name of %s" % fName)
+    quit()
+
+key_word = dict()
+for line in fHand:
+    line = line.strip()
+    words = line.split()
+    for word in words:
+        key_word[word] = "n/a"
+        print("Is %s in key_word? %s" % (word, word in key_word))
+
+print(key_word)
