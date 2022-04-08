0ad58a4e4f29a3fc330caff92e236667f1186041
bimonestle <bimonestle@yahoo.co.id>
2022-04-08 21:34:46 +0700

    add exercise brief

--- 10_tuples/exercise10.1.py
+++ 10_tuples/exercise10.1.py
@@ -1,3 +1,10 @@
+# Revise a previous program as follows: Read and parse the “From” lines and
+# pull out the addresses from the line. Count the number of messages from each person using a dictionary.
+
+# After all the data has been read, print the person with the most commits by
+# creating a list of (count, email) tuples from the dictionary. Then sort the list in
+# reverse order and print out the person who has the most commits.
+
 fName = input("Enter file name: ")
 try:
     fHandle = open(fName)
